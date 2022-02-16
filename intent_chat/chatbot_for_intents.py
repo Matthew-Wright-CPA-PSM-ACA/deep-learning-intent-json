619ae0adb30c7a2f21be92b39e551bcd1f29e97e
Matthew Wright, CPA, PSM, ACA <89610187+Matthew-Wright-CPA-PSM-ACA@users.noreply.github.com>
2022-02-16 14:36:13 +0100

    Add files via upload

---
+++ intent_chat/chatbot_for_intents.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# python chatbot_for_intents.py
+# please ensure to run python train_intents.py prior to running this
+
+from ai_model.prep_intents import *
+
+class chatBot:
+	def __init__(self):
+		self.exit_commands = ("quit", "exit", "stop")
+		
+	def prompt_user(self, text=None):
+		if text is None:
+			text = ""
+		return input(text + "\n~")
+
+	def commence_chat(self):
+		inputitem = input("I'm an A.I. that's trying to figure out the intent of your question or comment.\nPlease input something:\n~ ")
+		self.chat(inputitem)
+		return
+
+	def chat(self, reply):
+		while not self.depart(reply):
+			reply = input(predict(reply, quiet=True)+"\n~ ")
+		return
+
+	def depart(self, reply):
+		for exit_command in self.exit_commands:
+			if exit_command == reply: 
+				print("Well that was just swell. Till next time!\n\n")
+				return True
+		return False
